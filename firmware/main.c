d3a128e109fb16bdb6deb932d7f3a20a911d5188
jaseg <jaseg@jaseg.net>
2014-02-05 21:52:40 +0100

    Fixed license of main.c

--- firmware/main.c
+++ firmware/main.c
@@ -1,20 +1,5 @@
-/* Copyright (c) 2012 Texas Instruments Incorporated.  All rights reserved.
- * Software License Agreement
- * 
- * Texas Instruments (TI) is supplying this software for use solely and
- * exclusively on TI's microcontroller products. The software is owned by
- * TI and/or its suppliers, and is protected under applicable copyright
- * laws. You may not combine this software with "viral" open-source
- * software in order to form a larger program.
- * 
- * THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
- * NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
- * NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
- * CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
- * DAMAGES, FOR ANY REASON WHATSOEVER.
- * 
- * This is part of revision 9453 of the EK-LM4F120XL Firmware Package.
+/* Copyright (c) 2014 jaseg
+ * Released under GPLv3
  */
 
 #include "inc/hw_ints.h"
